d839e9ceb179149dce9cf11a916d1901a3655a97
Thomas Skjolberg <thomas.skjolberg@gmail.com>
2012-02-13 13:04:26 +0100

    Initial commit

---
+++ HelloWorld/src/com/antares/android/HelloWorldActivity.java
@@ -0,0 +1,13 @@
+package com.antares.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HelloWorldActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
