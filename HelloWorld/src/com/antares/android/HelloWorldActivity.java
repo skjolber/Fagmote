e3b98582bf52e876c9d2b0c3704454792341972e
Thomas Skjolberg <thomas.skjolberg@gmail.com>
2012-02-13 16:52:45 +0100

    updated project, readme

--- HelloWorld/src/com/antares/android/HelloWorldActivity.java
+++ HelloWorld/src/com/antares/android/HelloWorldActivity.java
@@ -1,7 +1,11 @@
 package com.antares.android;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class HelloWorldActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +13,21 @@ public class HelloWorldActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        // find the button instance
+        Button button = (Button) findViewById(R.id.button);
+        
+        // add a click listener to button
+        button.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+            		// log click
+                    Log.d(getClass().getSimpleName(), "Button was clicked!");
+                    
+                    // launch activity
+                    Intent i = new Intent(HelloWorldActivity.this, MyActivity.class);
+                    startActivity(i);
+            }
+        });
+
     }
 }
